0feed9ae02b2de116be493bb7056d38108106c59
A-nastasi-a <evsihneyeva@gmail.com>
2022-11-04 01:28:50 +0400

    feat: add solution to exersise 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathToText = path.join(__dirname, 'text.txt');
+
+const writeToFile = fs.createWriteStream(pathToText);
+const { stdin, stdout } = process;
+
+stdout.write('Приветствую!\nВведите что-нибудь:\n');
+stdin.on('data', data => {
+    const textData = data.toString();
+    if (textData.indexOf('exit') > -1) {
+        process.exit();
+    } else {
+        fs.appendFile(
+            pathToText,
+            textData,
+            (err) => {
+                if (err) throw err;
+            }
+        )
+    }
+});
+process.on('SIGINT', () => {
+    console.log('До свидания!');
+    process.exit();
+});
