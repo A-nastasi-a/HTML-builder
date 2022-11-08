8a7b0e83d422e4f8a62d4db510782441b3f6998b
A-nastasi-a <evsihneyeva@gmail.com>
2022-11-08 21:02:57 +0400

    fix: add cleaning folder before start to task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,6 +9,22 @@ fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
 
 const pathToResultDir = path.join(__dirname, 'files-copy');
 
+async function clearFolder() {
+    try {
+        const oldFiles = await fs.promises.readdir(path.join(__dirname, 'files-copy'), { withFileTypes: true });
+        for (const oldFile of oldFiles) {
+            fs.unlink(path.join(__dirname, 'files-copy', oldFile.name), err => {
+                if (err) throw err;
+            });
+        }
+        copyingFiles();
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
 async function copyingFiles() {
     try {
         const files = await fs.promises.readdir(pathToDir, { withFileTypes: true });
@@ -21,5 +37,5 @@ async function copyingFiles() {
         console.error(err);
     }
 };
-
-copyingFiles();
+clearFolder();
+// copyingFiles();
